93e69eae2b0e92d332289c962256461dc5a151db
Gaz1676 <gazfleming@gmail.com>
2017-11-21 12:30:12 +0000

    Imported user from models and injected picture into Schema

--- app/models/tweet.js
+++ app/models/tweet.js
@@ -7,12 +7,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const User = require('../models/user');
 
 // Module to represent a Schema for a Tweet model
+// https://hashnode.com/post/how-to-store-image-into-mongodb-using-nodejs-ciywexzlx000117539emkvcck
 
 const tweetSchema = mongoose.Schema({
   text: String,
   date: String,
+  picture: { data: Buffer, contentType: String },
   tweeter: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
