9196879a1f1f5311d195f377d47fe94cd7e5636d
Gaz1676 <gazfleming@gmail.com>
2017-12-19 15:11:06 +0000

    Added regex to payload for first/last name joi validation for register & update settings

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -12,207 +12,207 @@ const User = require('../models/user');
 
 // route to render main welcome page
 exports.main = {
-  auth: false,
-  handler: function (request, reply) {
-    reply.view('main', { title: 'Welcome to Twitter' });
-  },
-};
+    auth: false,
+    handler: function (request, reply) {
+        reply.view('main', { title: 'Welcome to Twitter' });
+      },
+  };
 
 
 // routes to render signup page
 exports.signup = {
-  auth: false,
-  handler: function (request, reply) {
-    reply.view('signup', { title: 'Sign up for Twitter' });
-  },
-};
+    auth: false,
+    handler: function (request, reply) {
+        reply.view('signup', { title: 'Sign up for Twitter' });
+      },
+  };
 
 
 // routes to render login page
 exports.login = {
-  auth: false,
-  handler: function (request, reply) {
-    reply.view('login', { title: 'Login to Twitter' });
-  },
-};
+    auth: false,
+    handler: function (request, reply) {
+        reply.view('login', { title: 'Login to Twitter' });
+      },
+  };
 
 
 // registers user to mongo database
 // error checking validation done through Joi
 // makes sure names are present & email not used & password length ok
 exports.register = {
-  validate: {
+    validate: {
 
-    payload: {
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      email: Joi.string().email({ unique: true }).required(),
-      password: Joi.string().min(6).max(20).required(),
-    },
+        payload: {
+            firstName: Joi.string().regex(/^[A-Z][a-z]{2,}$/).required(),
+            lastName: Joi.string().regex(/^[A-Z]/).min(3).required(),
+            email: Joi.string().email({ unique: true }).required(),
+            password: Joi.string().min(6).max(20).required(),
+          },
 
-    failAction: function (request, reply, source, error) {
-      reply.view('signup', {
-        title: 'Sign up error',
-        errors: error.data.details,
-      }).code(400);
-    },
-  },
+        failAction: function (request, reply, source, error) {
+            reply.view('signup', {
+                title: 'Sign up error',
+                errors: error.data.details,
+              }).code(400);
+          },
+      },
 
-  auth: false,
-  handler: function (request, reply) {
-    const user = new User(request.payload);
+    auth: false,
+    handler: function (request, reply) {
+        const user = new User(request.payload);
 
-    user.save().then(newUser => {
-      reply.redirect('/login');
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
-};
+        user.save().then(newUser => {
+            reply.redirect('/login');
+          }).catch(err => {
+            reply.redirect('/');
+          });
+      },
+  };
 
 
 // authenticates the user through validated login / password credentials
 // error checking validation done through Joi
 // makes sure a valid email / password is used
 exports.authenticate = {
-  validate: {
+    validate: {
 
-    payload: {
-      email: Joi.string().email({ unique: true }).required(),
-      password: Joi.string().min(6).max(20).required(),
-    },
-    failAction: function (request, reply, source, error) {
-      reply.view('login', {
-        title: 'Login error and now',
-        errors: error.data.details,
-      }).code(400);
-    },
-  },
+        payload: {
+            email: Joi.string().email({ unique: true }).required(),
+            password: Joi.string().min(6).max(20).required(),
+          },
+        failAction: function (request, reply, source, error) {
+            reply.view('login', {
+                title: 'Login error and now',
+                errors: error.data.details,
+              }).code(400);
+          },
+      },
 
-  auth: false,
-  handler: function (request, reply) {
-    const user = request.payload;
+    auth: false,
+    handler: function (request, reply) {
+        const user = request.payload;
 
-    if (user.email === 'tim@tom.com' && user.password === 'secret') {
-      request.cookieAuth.set({
-        loggedIn: true,
-        LoggedInUser: user.email,
-      });
-      reply.redirect('/adminhome');
-    } else {
-      User.findOne({ email: user.email }).then(user => {
-        if (user && user.password === user.password) {
+        if (user.email === 'tim@tom.com' && user.password === 'secret') {
           request.cookieAuth.set({
-            loggedIn: true,
-            loggedInUser: user._id,
-          });
-          reply.redirect('/globaltweets');
+              loggedIn: true,
+              LoggedInUser: user.email,
+            });
+          reply.redirect('/adminhome');
         } else {
-          reply.redirect('/signup');
+          User.findOne({ email: user.email }).then(user => {
+              if (user && user.password === user.password) {
+                request.cookieAuth.set({
+                    loggedIn: true,
+                    loggedInUser: user._id,
+                  });
+                reply.redirect('/globaltweets');
+              } else {
+                reply.redirect('/signup');
+              }
+            }).catch(err => {
+              reply.redirect('/');
+            });
         }
-      }).catch(err => {
-        reply.redirect('/');
-      });
-    }
-  },
-};
+      },
+  };
 
 
 // routes to render logout page
 // clears cookies
 exports.logout = {
-  auth: false,
-  handler: function (request, reply) {
-    request.cookieAuth.clear();
-    reply.redirect('/');
-  },
-};
+    auth: false,
+    handler: function (request, reply) {
+        request.cookieAuth.clear();
+        reply.redirect('/');
+      },
+  };
 
 
 // routes to render about page
 exports.about = {
-  auth: false,
-  handler: function (request, reply) {
-    reply.view('about', { title: 'About Twitter' });
-  },
-};
+    auth: false,
+    handler: function (request, reply) {
+        reply.view('about', { title: 'About Twitter' });
+      },
+  };
 
 
 // routes to render view settings page
 exports.viewSettings = {
-  handler: function (request, reply) {
-    let userId = request.auth.credentials.loggedInUser;
-    User.findOne({ _id: userId }).then(user => {
-      reply.view('settings', { title: 'Edit Account Settings', user: user });
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
-};
+    handler: function (request, reply) {
+        let userId = request.auth.credentials.loggedInUser;
+        User.findOne({ _id: userId }).then(user => {
+            reply.view('settings', { title: 'Edit Account Settings', user: user });
+          }).catch(err => {
+            reply.redirect('/');
+          });
+      },
+  };
 
 
 // routes to render update settings page
 // error checking validation done through Joi
 // makes sure names, email are present & password length ok
 exports.updateSettings = {
-  validate: {
+    validate: {
 
-    payload: {
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      email: Joi.string().email({ unique: true }).required(),
-      password: Joi.string().min(6).max(20).required(),
-    },
+        payload: {
+            firstName: Joi.string().regex(/^[A-Z][a-z]{2,}$/).required(),
+            lastName: Joi.string().regex(/^[A-Z]/).min(3).required(),
+            email: Joi.string().email({ unique: true }).required(),
+            password: Joi.string().min(6).max(20).required(),
+          },
 
-    failAction: function (request, reply, source, error) {
-      reply.view('settings', {
-        title: 'Settings error',
-        errors: error.data.details,
-      }).code(400);
-    },
-  },
-  handler: function (request, reply) {
-    const editedUser = request.payload;
-    let userId = request.auth.credentials.loggedInUser;
-    User.findOne({ _id: userId }).then(user => {
-      user.firstName = editedUser.firstName;
-      user.lastName = editedUser.lastName;
-      user.email = editedUser.email;
-      user.password = editedUser.password;
-      return user.save();
-    }).then(user => {
-      reply.view('settings', { title: 'Edit Account Settings', user: user });
-    });
-  },
-};
+        failAction: function (request, reply, source, error) {
+            reply.view('settings', {
+                title: 'Settings error',
+                errors: error.data.details,
+              }).code(400);
+          },
+      },
+    handler: function (request, reply) {
+        const editedUser = request.payload;
+        let userId = request.auth.credentials.loggedInUser;
+        User.findOne({ _id: userId }).then(user => {
+            user.firstName = editedUser.firstName;
+            user.lastName = editedUser.lastName;
+            user.email = editedUser.email;
+            user.password = editedUser.password;
+            return user.save();
+          }).then(user => {
+            reply.view('settings', { title: 'Edit Account Settings', user: user });
+          });
+      },
+  };
 
 
 // for uploading a users profile pic
-exports.profile_picture = {
+exports.profilePicture = {
 
-  handler: function (request, reply) {
-    let userId = request.auth.credentials.loggedInUser;
-    let userPic = request.payload.picture;
-    User.findOne({ _id: userId }).then(user => {
-      if (userPic.length) {
-        user.picture.data = userPic;
-        user.save();
-      }
-      reply.redirect('/settings');
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
-};
+    handler: function (request, reply) {
+        let userId = request.auth.credentials.loggedInUser;
+        let userPic = request.payload.picture;
+        User.findOne({ _id: userId }).then(user => {
+            if (userPic.length) {
+              user.picture.data = userPic;
+              user.save();
+            }
+            reply.redirect('/settings');
+          }).catch(err => {
+            reply.redirect('/');
+          });
+      },
+  };
 
 
 exports.getUserPicture = {
-  handler: function (request, reply) {
-    let userId = request.params.id;
-    User.findOne({ _id: userId }).then(user => {
-      reply(user.picture.data).type('image');
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
-};
+    handler: function (request, reply) {
+        let userId = request.params.id;
+        User.findOne({ _id: userId }).then(user => {
+            reply(user.picture.data).type('image');
+          }).catch(err => {
+            reply.redirect('/');
+          });
+      },
+  };
