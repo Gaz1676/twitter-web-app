c7fa3dc6181b81cf124369eaa2c08c2c13919d73
Gaz1676 <gazfleming@gmail.com>
2017-12-22 21:03:59 +0000

    Created sticky menu for nav bar on user and admin screens

---
+++ public/js/sticky-menu.js
@@ -0,0 +1,13 @@
+// https://jsfiddle.net/reimeister/8kGE5/
+
+let win      = $(window),
+    fxel     = $('nav'),
+    eloffset = fxel.offset().top;
+
+win.scroll(function () {
+    if (eloffset < win.scrollTop()) {
+      fxel.addClass('fixed');
+    } else {
+      fxel.removeClass('fixed');
+    }
+  });
