0c7c660a1e7a07c96cbd1bbba87a7e2efc108578
Gaz1676 <gazfleming@gmail.com>
2018-01-02 21:12:57 +0000

    Set up api's to connect to android (not working on android end)

--- app/api/tweetsapi.js
+++ app/api/tweetsapi.js
@@ -1,79 +1,77 @@
-// Created to expose endpoints from the application
-
 const Tweet = require('../models/tweet');
 const Boom = require('boom');
+const utils = require('./utils.js');
+
+
+exports.find = {
+
+    auth: false,
+    handler: function (request, reply) {
+        Tweet.find({}).exec().then(tweets => {
+            reply(tweets);
+          }).catch(err => {
+            reply(Boom.badImplementation('error accessing db'));
+          });
+      },
+
+  };
+
+
+exports.findOne = {
+
+    auth: false,
+    handler: function (request, reply) {
+        Tweet.findOne({ _id: request.params.id }).then(tweet => {
+            if (tweet != null) {
+              reply(tweet);
+            } else {
+              reply(Boom.notFound('id not found'));
+            }
+          }).catch(err => {
+            reply(Boom.notFound('id not found'));
+          });
+      },
+  };
 
 
 exports.create = {
 
     auth: false,
-
     handler: function (request, reply) {
         const tweet = new Tweet(request.payload);
         tweet.save().then(newTweet => {
             reply(newTweet).code(201);
-        }).catch(err => {
+          }).catch(err => {
             reply(Boom.badImplementation('error creating tweet'));
-        });
-    },
+          });
+      },
 
-};
+  };
 
-exports.findOne = {
-
-    auth: false,
-
-    handler: function (request, reply) {
-        Tweet.findOne({ _id: request.params.id }).then(tweet => {
-            if (tweet != null) {
-                reply(tweet);
-            } else {
-                reply(Boom.notFound('id not found'));}
-        }).catch(err => {
-            reply(Boom.notFound('id not found'));
-        });
-    },
-
-};
-
-exports.findAll = {
-
-    auth: false,
-
-    handler: function (request, reply) {
-        Tweet.find({}).exec().then(tweets => {
-            reply(tweets);
-        }).catch(err => {
-            reply(Boom.badImplementation('error accessing db'));
-        });
-    },
-
-};
-
-exports.deleteOne = {
-
-    auth: false,
-
-    handler: function (request, reply) {
-        Tweet.remove({ _id: request.params.id }).then(tweet => {
-            reply(tweet).code(204);
-        }).catch(err => {
-            reply(Boom.notFound('id not found'));
-        });
-    },
-
-};
 
 exports.deleteAll = {
 
     auth: false,
-
     handler: function (request, reply) {
         Tweet.remove({}).then(err => {
             reply().code(204);
-        }).catch(err => {
+          }).catch(err => {
             reply(Boom.badImplementation('error removing tweets'));
-        });
-    },
+          });
+      },
 
-};
+  };
+
+
+exports.deleteOne = {
+
+    auth: false,
+    handler: function (request, reply) {
+        Tweet.remove({ _id: request.params.id }).then(tweet => {
+            reply().code(204);
+          }).catch(err => {
+            reply(Boom.notFound('id not found'));
+          });
+      },
+
+  };
--- app/api/usersapi.js
+++ app/api/usersapi.js
@@ -1,79 +1,97 @@
-// Created to expose endpoints from the application
+'use strict';
 
 const User = require('../models/user');
 const Boom = require('boom');
+const utils = require('./utils.js');
 
-exports.create = {
+
+exports.find = {
 
     auth: false,
-
     handler: function (request, reply) {
-        const user = new User(request.payload);
-        user.save().then(newUser => {
-            reply(newUser).code(201);
-        }).catch(err => {
-            reply(Boom.badImplementation('error creating user'));
-        });
-    },
+        User.find({}).exec().then(users => {
+            reply(users);
+          }).catch(err => {
+            reply(Boom.badImplementation('error accessing db'));
+          });
+      },
+  };
 
-};
 
 exports.findOne = {
 
     auth: false,
-
     handler: function (request, reply) {
         User.findOne({ _id: request.params.id }).then(user => {
-            if (user !== null) {
-                reply(user);
+            if (user != null) {
+              reply(user);
             } else {
-                reply(Boom.notFound('id not found'));
+              reply(Boom.notFound('id not found'));
             }
-        }).catch(err => {
+          }).catch(err => {
             reply(Boom.notFound('id not found'));
-        });
-    },
+          });
+      },
+  };
 
-};
 
-exports.findAll = {
+exports.create = {
 
     auth: false,
-
     handler: function (request, reply) {
-        User.find({}).exec().then(users => {
-            reply(users);
-        }).catch(err => {
-            reply(Boom.badImplementation('error accessing db'));
-        });
-    },
+        const user = new User(request.payload);
+        user.save().then(newUser => {
+            reply(newUser).code(201);
+          }).catch(err => {
+            reply(Boom.badImplementation('error creating User'));
+          });
+      },
+  };
 
-};
 
 exports.deleteAll = {
 
     auth: false,
-
     handler: function (request, reply) {
         User.remove({}).then(err => {
             reply().code(204);
-        }).catch(err => {
-            reply(Boom.badImplementation('error removing users'));
-        });
-    },
+          }).catch(err => {
+            reply(Boom.badImplementation('error removing Users'));
+          });
+      },
+  };
 
-};
 
 exports.deleteOne = {
 
     auth: false,
-
     handler: function (request, reply) {
         User.remove({ _id: request.params.id }).then(user => {
-            reply(user).code(204);
-        }).catch(err => {
+            reply(User).code(204);
+          }).catch(err => {
             reply(Boom.notFound('id not found'));
-        });
-    },
+          });
+      },
+  };
 
-};
+
+exports.authenticate = {
+
+    auth: false,
+    handler: function (request, reply) {
+        const user = request.payload;
+        User.findOne({ email: user.email }).then(foundUser => {
+            if (foundUser && foundUser.password === user.password) {
+              const token = utils.createToken(foundUser);
+              reply({ success: true, token: token, user: foundUser }).code(201);
+            } else {
+              reply({
+                  success: false,
+                  message: 'Authentication failed. User not found.',
+                }).code(201);
+            }
+          }).catch(err => {
+            reply(Boom.notFound('internal db failure'));
+          });
+      },
+  };
