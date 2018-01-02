17c8af222b9a2bcc97aa9a8ac75ea1b793671737
Gaz1676 <gazfleming@gmail.com>
2018-01-02 22:18:26 +0000

    Updated file with jwt authentication

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-/**
- * Author: Gary Fleming
- * Student No: 20019497
- * Start Date: Oct 10th 2017
- */
-
 'use strict';
 
 const Hapi = require('hapi');
-
+const corsHeaders = require('hapi-cors-headers');
+const utils = require('./app/api/utils');
 
 let server = new Hapi.Server();
+
+
 server.connection({
     port: process.env.PORT || 4000,
     routes: {
@@ -17,7 +14,7 @@ server.connection({
         // https://stackoverflow.com/questions/45627919/how-to-set-max-image-size-in-joi-hapi
         payload: {
             maxBytes: 1000 * 1000 * 5, // increases to 5mb
-        },
+          },
 
         // validation: reports all errors and not just one
         // set in routes so it will be used across all files (DRY)
@@ -25,65 +22,69 @@ server.connection({
 
             options: {
                 abortEarly: false,
-            },
-        },
-    },
-});
+              },
+          },
+      },
+  });
 
-
-
+// import of the db just created
 require('./app/models/db');
 
+// plugins registration
+server.register([require('inert'), require('vision'), require('hapi-auth-cookie'), require('hapi-auth-jwt2')], err => {
 
-server.register([require('inert'), require('vision'), require('hapi-auth-cookie')], err => {
     if (err) {
-        throw err;
+      throw err;
     }
-
 
     // assign handlebars engine for views to the server
     server.views({
         engines: {
             hbs: require('handlebars'),
-        },
+          },
         relativeTo: __dirname,
         path: './app/views',
         layoutPath: './app/views/layout',
         partialsPath: './app/views/partials',
         layout: true,
         isCached: false,
-    });
+      });
 
-
-    // defining cookie strategy
+    // cookie plugin
     server.auth.strategy('standard', 'cookie', {
         password: 'secretpasswordnotrevealedtoanyone',
-        cookie: 'twitter-cookie',
+        cookie: 'donation-cookie',
         isSecure: false,
         ttl: 24 * 60 * 60 * 1000,
         redirectTo: '/login',
-    });
+      });
 
-
-    // cookie set as strategy for all routes.
-    // app will be disabled as all routes are protected (looking for a cookie)
-    // some routes have set auth: false to counter this and stop app from crashing
+    // strategy set to all routes
     server.auth.default({
         strategy: 'standard',
-    });
+      });
 
+    server.auth.strategy('jwt', 'jwt', {
+        key: 'secretpasswordnotrevealedtoanyone',
+        validateFunc: utils.validate,
+        verifyOptions: { algorithms: ['HS256'] },
+      });
 
+    // enable the facility with default options
+    server.ext('onPreResponse', corsHeaders);
+
+    // route into the application server
     server.route(require('./routes'));
 
-
-    // defines the endpoints from api folder
+    // new api route included into the application server
     server.route(require('./routesapi'));
-
 
     server.start((err) => {
         if (err) {
-            throw err;
+          throw err;
         }
-        console.log('You are tuned into:', server.info.uri, ' - hope you enjoy!!');
-    });
-});
+
+        console.log('Server listening at:', server.info.uri);
+      });
+
+  });
